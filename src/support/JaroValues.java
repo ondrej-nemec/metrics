c56b6f6c233feed35433abe70540bba4bce1d822
Ondřej Němec <nikdynikde@gmail.com>
2018-05-13 20:14:41 +0200

    Added JaroValues

---
+++ src/support/JaroValues.java
@@ -0,0 +1,12 @@
+package support;
+
+/**
+ * Values for Jaro distance matrix
+ * @author Ondøej Nìmec
+ *
+ */
+public enum JaroValues {
+	NULL,  //out of space
+	TRUE,  //same values
+	FALSE  //different values
+}
