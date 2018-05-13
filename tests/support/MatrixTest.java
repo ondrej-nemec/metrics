11c8aba194ed2f02d2e340195a2d7e89aedac194
Ondřej Němec <nikdynikde@gmail.com>
2018-05-13 20:33:10 +0200

    Added MatrixTest

---
+++ tests/support/MatrixTest.java
@@ -0,0 +1,30 @@
+package support;
+
+import org.junit.Test;
+
+import support.Matrix;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest{
+	
+	private Matrix<Integer> matrix;
+	
+	public MatrixTest() {
+		Integer[][] inte = new Integer[1][1];
+		inte[0][0] = 1;
+		this.matrix = new Matrix<>(inte);
+	}
+	
+	/*
+	@Test
+	public void matrixGetThrowsException(){
+		
+	}
+	*/
+	@Test
+	public void matrixGetWork(){
+		assertSame(1, matrix.getCell(0, 0));
+	}
+
+}
