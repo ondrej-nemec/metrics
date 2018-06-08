0c4953f54438119b0e61dbca789af9bb485fcf1f
Ondřej Němec <nikdynikde@gmail.com>
2018-06-08 22:57:29 +0200

    JaroWinklerQuick implemented

--- src/metricquick/JaroWinklerQuick.java
+++ src/metricquick/JaroWinklerQuick.java
@@ -4,9 +4,25 @@ import java.util.List;
 
 public class JaroWinklerQuick<S> implements MetricQuick<S> {
 
+	private double p;
+	
 	@Override
 	public Number calculate(List<S> sequenceFrom, List<S> sequenceTo) {
-		throw new UnsupportedOperationException();
+		double jaro = new JaroQuick<S>().calculate(sequenceFrom, sequenceTo).doubleValue();
+		return jaro +
+				(getAlfa(sequenceFrom, sequenceTo) * p *
+						(1 - jaro)
+						);
 	}
+	
+	private int getAlfa(List<S> from, List<S> to){
+		int result = 0;
+		for(int i = 0; i < Math.min(4, Math.min(from.size(), to.size())); i++){
+			if(from.get(i).equals(to.get(i)))
+				result++;
+		}
+		return result;
+	}
+	
 
 }
