8fc903c3259e0c9d15a57ab9ae944be68736e66a
Ondřej Němec <nikdynikde@gmail.com>
2018-06-16 22:28:41 +0200

    WIP - jaro info test

--- tests/metricinfo/JaroInfoTest.java
+++ tests/metricinfo/JaroInfoTest.java
@@ -45,77 +45,212 @@ public class JaroInfoTest {
 					new Object[]{
 							Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
 							Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
-							1.0
+							new ResultSet<>(
+									Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+									Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+									Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+									Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+									"Jaro distance",
+									"EEEEEE",
+									1.0,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('s', 'l', 'o', 'v', 'o'),
 							new LinkedList<>(),
-							0.0
+							new ResultSet<>(
+									Arrays.asList('s', 'l', 'o', 'v', 'o'),
+									new LinkedList<>(),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.0,
+									null
+								)
 					},
 					new Object[]{
 							new LinkedList<>(),
 							Arrays.asList('s', 'l', 'o', 'v', 'o'),
-							0.0
+							new ResultSet<>(
+									new LinkedList<>(),
+									Arrays.asList('s', 'l', 'o', 'v', 'o'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.0,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('s', 'l', 'o', 'v', 'o'),
 							Arrays.asList('l'),
-							0.7333333333333333
+							new ResultSet<>(
+									Arrays.asList('s', 'l', 'o', 'v', 'o'),
+									Arrays.asList('l'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.7333333333333333,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('s', 'l', 'o', 'v', 'o'),
 							Arrays.asList('s', 'l'),
-							0.8
+							new ResultSet<>(
+									Arrays.asList('s', 'l', 'o', 'v', 'o'),
+									Arrays.asList('s', 'l'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.8,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('s', 'l', 'o', 'v', 'a'),
 							Arrays.asList('a'),
-							0.0
+							new ResultSet<>(
+									Arrays.asList('s', 'l', 'o', 'v', 'a'),
+									Arrays.asList('a'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.0,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('o', 'k', 'n', 'o'),
 							Arrays.asList('w', 'i', 'n', 'd', 'o', 'w'),
-							0.6111111111111112
+							new ResultSet<>(
+									Arrays.asList('o', 'k', 'n', 'o'),
+									Arrays.asList('w', 'i', 'n', 'd', 'o', 'w'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.6111111111111112,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('k', 'o', 'l', 'o'),
 							Arrays.asList('o', 'k', 'o'),
-							0.8055555555555555
+							new ResultSet<>(
+									Arrays.asList('k', 'o', 'l', 'o'),
+									Arrays.asList('o', 'k', 'o'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.8055555555555555,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('n', 'e', 'n', 'í'),
 							Arrays.asList('n', 'i', 'e', ' ', 'j', 'e'),
-							0.6111111111111112
+							new ResultSet<>(
+									Arrays.asList('n', 'e', 'n', 'í'),
+									Arrays.asList('n', 'i', 'e', ' ', 'j', 'e'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.6111111111111112,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('h', 'o', 'u', 's', 'k', 'a'),
 							Arrays.asList('h', 'o', 'u', 's', 'l', 'e'),
-							0.7777777777777777
+							new ResultSet<>(
+									Arrays.asList('h', 'o', 'u', 's', 'k', 'a'),
+									Arrays.asList('h', 'o', 'u', 's', 'l', 'e'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.7777777777777777,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
 							Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),
-							0.75
+							new ResultSet<>(
+									Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
+									Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.75,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('k', 'o', 'p', 'l', 'i', 'n', 'n'),
 							Arrays.asList('k', 'p', 'o', 'l', 'l', 'i', 'm'),
-							0.7428571428571429
+							new ResultSet<>(
+									Arrays.asList('k', 'o', 'p', 'l', 'i', 'n', 'n'),
+									Arrays.asList('k', 'p', 'o', 'l', 'l', 'i', 'm'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.7428571428571429,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('w', 'o', 'r', 'd'),
 							Arrays.asList('7', ';', '$', 'Ð'),
-							0.0
+							new ResultSet<>(
+									Arrays.asList('w', 'o', 'r', 'd'),
+									Arrays.asList('7', ';', '$', 'Ð'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.0,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('a', 'b', 'b', 'c', 'b'),
 							Arrays.asList('a', 'b', 'c', 'a', 'b'),
-							0.8666666666666667
+							new ResultSet<>(
+									Arrays.asList('a', 'b', 'b', 'c', 'b'),
+									Arrays.asList('a', 'b', 'c', 'a', 'b'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.8666666666666667,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('a', 'a', 'h', 'o', 'j'),
 							Arrays.asList('a', 'h', 'o', 'j', 'k', 'y'),
-							0.8222222222222222
+							new ResultSet<>(
+									Arrays.asList('a', 'a', 'h', 'o', 'j'),
+									Arrays.asList('a', 'h', 'o', 'j', 'k', 'y'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.8222222222222222,
+									null
+								)
 					},
 					new Object[]{
 							Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
@@ -124,7 +259,20 @@ public class JaroInfoTest {
 							Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
 										  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
 										  'á', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
-							0.8787878787878787
+							new ResultSet<>(
+									Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
+											  'ì', 'p', 'o', 'd', 'o', 'b', 'ò', 'o', 'v', 'á', 'v',
+											  'a', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
+									Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
+											  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
+											  'á', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
+									null,
+									null,
+									"Jaro distance",
+									"",
+									0.8787878787878787,
+									null
+								)
 					}				
 				);
 	}
