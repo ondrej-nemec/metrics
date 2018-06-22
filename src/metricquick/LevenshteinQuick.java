3e009c4c1ea0a8353980e9c80c54b9ad2583ecbd
Ondřej Němec <nikdynikde@gmail.com>
2018-06-22 21:25:38 +0200

    LevenshteinQuick faster

--- src/metricquick/LevenshteinQuick.java
+++ src/metricquick/LevenshteinQuick.java
@@ -6,31 +6,17 @@ public class LevenshteinQuick<S> implements MetricQuick<S> {
 
 	@Override
 	public Number calculate(List<S> sequenceFrom, List<S> sequenceTo) {
-		//List<Integer> upper = new ArrayList<>();
 		int[] upper = new int[sequenceTo.size()+1];
 		for(int i = 0; i <= sequenceTo.size(); i++){
-			//upper.add(i);
 			upper[i] = i;
 		}
-		//List<Integer> actual = new ArrayList<>();
 		int[] actual = new int[sequenceTo.size()+1];
 		for(int row = 1; row <= sequenceFrom.size(); row++){
-			//actual.add(row);
 			actual[0] = row;
 			for(int col = 1; col <= sequenceTo.size(); col++){
 				int bool = 1;
 				if(sequenceFrom.get(row-1).equals(sequenceTo.get(col-1)))
 					bool--;
-				/*
-				actual.add(
-						Math.min(upper.get(col-1) + bool,
-								Math.min(
-										upper.get(col) + 1,
-										actual.get(col-1) + 1
-										)
-								)
-						);
-				*/
 				actual[col] = 
 						Math.min(upper[col-1] + bool,
 								Math.min(
@@ -40,11 +26,9 @@ public class LevenshteinQuick<S> implements MetricQuick<S> {
 								);
 			}
 			upper = actual;
-			//actual = new ArrayList<>();
 			actual = new int[sequenceTo.size()+1];
 			
 		}
-		//return upper.get(sequenceTo.size());
 		return upper[sequenceTo.size()];
 	}
 
