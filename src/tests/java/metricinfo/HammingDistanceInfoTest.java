e32ba1095ad167b78e78f9f7e4115f6ea61d0465
Ondřej Němec <nikdynikde@gmail.com>
2018-10-10 22:09:27 +0200

    new dataset jaro winkler distance info test

--- src/tests/java/metricinfo/HammingDistanceInfoTest.java
+++ src/tests/java/metricinfo/HammingDistanceInfoTest.java
@@ -41,7 +41,7 @@ public class HammingDistanceInfoTest extends AbstractMetricInfoTest {
 	}
 	
 	public Object[] parametersForTestWeightDistance() {
-		return makeParams(weightDistances());
+		return parameters(3);
 	}
 
 	@Test
@@ -55,8 +55,10 @@ public class HammingDistanceInfoTest extends AbstractMetricInfoTest {
 	}
 	
 	public Object[] parametersForTestCalculateWorks() {
-		return makeParams(resultSets());
+		return parameters(2);
 	}	
+	
+	/********************************/
 	
 	private ResultSet<Character, String> makeResultSet(
 			List<Character> from,
@@ -74,98 +76,114 @@ public class HammingDistanceInfoTest extends AbstractMetricInfoTest {
 	}
 
 	/************************************/
-	
-	public Object[] resultSets() {
-		return new Object[]{
-				makeResultSet(
-						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
-						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
-						"EEEEEE",
-						0
+		
+	@Override
+	public List<Object[]> resultSet() {
+		return Arrays.asList(
+				//equals
+				new Object[]{
+					Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+					Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+					makeResultSet(
+							Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+							Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+							"EEEEEE",
+							0
 					),
-				makeResultSet(
-						Arrays.asList('h', 'o', 'u', 's', 'k', 'a'),
-						Arrays.asList('h', 'o', 'u', 's', 'l', 'e'),
-						"EEEESS",
-						2
+					0
+				},
+				//different
+				new Object[]{
+					Arrays.asList('a', 'a', 'a'),
+					Arrays.asList('b', 'b', 'b'),
+					makeResultSet(
+							Arrays.asList('a', 'a', 'a'),
+							Arrays.asList('b', 'b', 'b'),
+							"SSS",
+							3
+						),
+					6
+				},
+				//substitution
+				new Object[]{
+					Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+					Arrays.asList('s', 't', 'r', 'y', 'n', 'g'),
+					makeResultSet(
+							Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+							Arrays.asList('s', 't', 'r', 'y', 'n', 'g'),
+							"EEESEE",
+							1
+						),
+					2
+				},
+				//insertion and deletion
+				new Object[]{
+						Arrays.asList('s', 't', 'r', 'n', 'g', 'a'),
+						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+						makeResultSet(
+								Arrays.asList('s', 't', 'r', 'n', 'g', 'a'),
+								Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+								"EEESSS",
+								3
+							),
+						6
+					},		
+				//transposition
+				new Object[]{
+						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+						Arrays.asList('t', 's', 'r', 'i', 'n', 'g'),
+						makeResultSet(
+								Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
+								Arrays.asList('t', 's', 'r', 'i', 'n', 'g'),
+								"SSEEEE",
+								2
+							),
+						4
+					},				
+				//so long sequence
+				new Object[]{
+					Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
+								  'Ä›', 'p', 'o', 'd', 'o', 'b', 'Åˆ', 'o', 'v', 'Ã¡', 'v',
+								  'a', 't', 'e', 'l', 'n', 'Ä›', 'j', 'Å¡', 'Ã­', 'h', 'o'),
+					Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
+								  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
+								  'Ã¡', 't', 'e', 'l', 'n', 'Ä›', 'j', 'Å¡', 'Ã­', 'h', 'o'),
+					makeResultSet(
+							Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
+										  'Ä›', 'p', 'o', 'd', 'o', 'b', 'Åˆ', 'o', 'v', 'Ã¡', 'v',
+										  'a', 't', 'e', 'l', 'n', 'Ä›', 'j', 'Å¡', 'Ã­', 'h', 'o'),
+							Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
+										  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
+										  'Ã¡', 't', 'e', 'l', 'n', 'Ä›', 'j', 'Å¡', 'Ã­', 'h', 'o'),
+							"EESSSSSSSSSSEEEEESEESESEEEEEEEEEE",
+							13
 					),
-				makeResultSet(
-						Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
-						Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),
-						"EESESSSS",
-						5
-					),	
-				makeResultSet(
+					26
+				},
+				//koolipan vs kopllisa
+				new Object[]{
+					Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
+					Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),
+					makeResultSet(
+							Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
+							Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),
+							"EESESSSS",
+							5
+						),
+					10
+				},				
+				//kolinn vs kpollim
+				new Object[]{
 						Arrays.asList('k', 'o', 'p', 'l', 'i', 'n', 'n'),
 						Arrays.asList('k', 'p', 'o', 'l', 'l', 'i', 'm'),
-						"ESSESSS",
-						5
-					),
-				makeResultSet(
-						Arrays.asList('w', 'o', 'r', 'd'),
-						Arrays.asList('7', ';', '$', 'Ð'),
-						"SSSS",
-						4
-					),
-				makeResultSet(
-						Arrays.asList('a', 'b', 'b', 'c', 'b'),
-						Arrays.asList('a', 'b', 'c', 'a', 'b'),
-						"EESSE",
-						2
-					),
-				makeResultSet(
-						Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
-								  'ì', 'p', 'o', 'd', 'o', 'b', 'ò', 'o', 'v', 'á', 'v',
-								  'a', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
-						Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
-								  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
-								  'á', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
-						"EESSSSSSSSSSEEEEESEESESEEEEEEEEEE",
-						13
-					),	
-		};
-	}
-	
-	public List<Object[]> sequences() {
-		return Arrays.asList(
-				new Object[]{
-						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),
-						Arrays.asList('s', 't', 'r', 'i', 'n', 'g'),						
-				},
-				new Object[]{
-						Arrays.asList('h', 'o', 'u', 's', 'k', 'a'),
-						Arrays.asList('h', 'o', 'u', 's', 'l', 'e'),					
-				},
-				new Object[]{
-						Arrays.asList('k', 'o', 'o', 'l', 'i', 'p', 'a', 'n'),
-						Arrays.asList('k', 'o', 'p', 'l', 'l', 'i', 's', 'a'),						
-				},
-				new Object[]{
-						Arrays.asList('k', 'o', 'p', 'l', 'i', 'n', 'n'),
-						Arrays.asList('k', 'p', 'o', 'l', 'l', 'i', 'm'),						
-				},
-				new Object[]{
-						Arrays.asList('w', 'o', 'r', 'd'),
-						Arrays.asList('7', ';', '$', 'Ð'),							
-				},
-				new Object[]{
-						Arrays.asList('a', 'b', 'b', 'c', 'b'),
-						Arrays.asList('a', 'b', 'c', 'a', 'b'),						
-				},
-				new Object[]{
-						Arrays.asList('n', 'e', 'j', 'n', 'e', 'z', 'p', 'r', 'a', 'v', 'd',
-								  'ì', 'p', 'o', 'd', 'o', 'b', 'ò', 'o', 'v', 'á', 'v',
-								  'a', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
-						Arrays.asList('n', 'e', 'n', 'e', 's', 'p', 'r', 'a', 'v', 'd', 'j',
-								  'e', 'p', 'o', 'd', 'o', 'b', 'n', 'o', 'v', 'a', 'v',
-								  'á', 't', 'e', 'l', 'n', 'ì', 'j', 'š', 'í', 'h', 'o'),
+						makeResultSet(
+								Arrays.asList('k', 'o', 'p', 'l', 'i', 'n', 'n'),
+								Arrays.asList('k', 'p', 'o', 'l', 'l', 'i', 'm'),
+								"ESSESSS",
+								5
+							),
+						10
 				}
 			);
-	}
-	
-	public Object[] weightDistances() {
-		return new Object[]{
-				0, 4, 10, 10, 8, 4, 26,
-		};
 	}
 }
