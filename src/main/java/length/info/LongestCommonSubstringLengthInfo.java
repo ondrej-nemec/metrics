68b2852c7a624614e895bcc993a5abe1ce13a11a
Ondřej Němec <nikdynikde@gmail.com>
2018-10-24 22:20:33 +0200

    WIP - longest common substring info

---
+++ src/main/java/length/info/LongestCommonSubstringLengthInfo.java
@@ -0,0 +1,16 @@
+package length.info;
+
+import java.util.List;
+
+import structures.LengthMatrixResult;
+import structures.LengthResult;
+
+public class LongestCommonSubstringLengthInfo<S> implements StructureMatrixLength<S, Integer>{
+
+	@Override
+	public LengthResult<S, LengthMatrixResult<Integer>> calculate(List<S> sequenceFrom, List<S> sequenceTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
